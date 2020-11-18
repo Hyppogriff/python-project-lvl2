273386c9128c4fde7289238f1dfa9d6879b8b5ff
Psyker <hyppogriff@gmail.com>
2020-11-18 21:44:40 +0300

    update -format flag

--- scripts/gendiff.py
+++ scripts/gendiff.py
@@ -3,7 +3,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('-f, --format', metavar='FORMAT',
+    parser.add_argument('-f', '--format', metavar='FORMAT',
                         help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
